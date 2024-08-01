5f36244fbbab17f17377ed3d0f1710689b6740f3
Ahmad Syarif <88640279+syarif0@users.noreply.github.com>
2024-08-01 09:25:45 +0700

    Update scan.js

--- Front-End/pages/scan.js
+++ Front-End/pages/scan.js
@@ -5,8 +5,8 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import styles from "../styles/Home.module.css";
 
-const supabaseUrl = 'https://gauxhtapvigvxlbsjmbv.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhdXhodGFwdmlndnhsYnNqbWJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwNzMyMDEsImV4cCI6MjAzMjY0OTIwMX0.HKu0bu4807rVC7R53IMdZLwkTHy2DX1LHkwCZpg7PVI';
+const supabaseUrl = 'https://hosturl.supabase.co';
+const supabaseKey = 'key';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 function Scan() {
